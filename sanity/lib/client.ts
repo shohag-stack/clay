c2a9abe59eb8f4712672b672948dae1a532c9177
shohag-stack <shohag4y@gmail.com>
2026-04-17 12:33:52 +0600

    client fixed

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,9 +1,14 @@
-// sanity/lib/client.ts
 import { createClient } from "next-sanity";
 
-export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "demo",
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  apiVersion: "2024-01-01",
-  useCdn: true,
-});
+const isSanityReady =
+  !!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID &&
+  !!process.env.NEXT_PUBLIC_SANITY_DATASET;
+
+export const client = isSanityReady
+  ? createClient({
+      projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+      dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+      apiVersion: "2024-01-01",
+      useCdn: true,
+    })
+  : null;
